ec22af01d5a8490111a9b80aa06dbe2d445d75ce
Nathaniel Coulter <nathaniel.coulter21@my.stjohns.edu>
2026-05-07 19:29:21 -0400

    Add files via upload

--- scripts/pull_usdngn_yfinance.py
+++ scripts/pull_usdngn_yfinance.py
@@ -1,16 +1,17 @@
-# pull_usdngn_yfinance.py
+# pull_fx_yfinance.py
+# This script just pulls the USD to (NGN) FX rates from yfinance Api. All you have to do is change the second currency and it works with all currencies not just african.
 
 import yfinance as yf
 from pathlib import Path
 
-TICKER = "USDNGN=X"
-PERIOD = "5y"
+TICKER = "USDMAD=X" #change "MAD" to the currency of your choice
+PERIOD = "5y" #change "5y" to the time period you want
 INTERVAL = "1d"
 
 out_dir = Path("data")
 out_dir.mkdir(exist_ok=True)
 
-out_file = out_dir / "usdngn_5y_daily.csv"
+out_file = out_dir / "usdmad_5y_daily.csv" # change the "5y" to whatever time period you're pulling just so the CSV file is named correctly :)
 
 df = yf.download(
     TICKER,
@@ -23,7 +24,6 @@ df = yf.download(
 if df.empty:
     raise ValueError("No data returned from yfinance.")
 
-# Flatten columns if yfinance returns multi-index columns
 if isinstance(df.columns, tuple) or hasattr(df.columns, "levels"):
     df.columns = [col[0] if isinstance(col, tuple) else col for col in df.columns]
 
